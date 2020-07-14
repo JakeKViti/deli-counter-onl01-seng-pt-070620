2e9e7e966d44acd35fb7b5dededfc6fd9de7bc6f
Jacob Viti <thevitij64@gmail.com>
2020-07-14 00:53:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line(array)
+  spot = 0
+ if array.length < 1
+   puts "The line is currently empty."
+ else
+   array.each do |name|
+   puts "The line is currently: #{spot}. #{name}."
+   spot += 1
+ end
+ end
+end
+
+def take_a_number(array, name)
+
+end
+
+def now_serving(array)
+
+end
